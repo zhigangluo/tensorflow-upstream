e3a39c7a64621113b7e31530f128f7f873c8d6c4
Wen-Heng (Jack) Chung <whchung@gmail.com>
2018-08-01 08:50:13 -0500

    resolve conflicts for PR#91

--- tensorflow/tools/ci_build/linux/rocm/run_py3_core.sh
+++ tensorflow/tools/ci_build/linux/rocm/run_py3_core.sh
@@ -40,22 +40,22 @@ bazel test --test_sharding_strategy=disabled --config=rocm --test_tag_filters=-n
     --build_tests_only --test_output=errors --local_test_jobs=${TF_GPU_COUNT} --config=opt \
     --run_under=//tensorflow/tools/ci_build/gpu_build:parallel_gpu_execute -- \
     //tensorflow/... -//tensorflow/compiler/... -//tensorflow/contrib/... \
-    -//tensorflow/python:optimizer_test \
-    -//tensorflow/python/estimator:boosted_trees_test   \
-    -//tensorflow/python/estimator:dnn_linear_combined_test   \
-    -//tensorflow/python/feature_column:feature_column_test \
+    -//tensorflow/python/eager:pywrap_tfe_test \
+    -//tensorflow/python/estimator:estimator_test \
     -//tensorflow/python/keras:activations_test \
+    -//tensorflow/python/keras:cudnn_recurrent_test \
     -//tensorflow/python/keras:normalization_test \
     -//tensorflow/python/keras:pooling_test \
+    -//tensorflow/python/keras:training_gpu_test \
     -//tensorflow/python/kernel_tests:atrous_conv2d_test \
     -//tensorflow/python/kernel_tests:bincount_op_test \
     -//tensorflow/python/kernel_tests:concat_op_test \
     -//tensorflow/python/kernel_tests:control_flow_ops_py_test \
-    -//tensorflow/python/kernel_tests:conv_ops_3d_test \
-    -//tensorflow/python/kernel_tests:conv_ops_test \
     -//tensorflow/python/kernel_tests:conv1d_test \
     -//tensorflow/python/kernel_tests:conv2d_backprop_filter_grad_test \
     -//tensorflow/python/kernel_tests:conv2d_transpose_test \
+    -//tensorflow/python/kernel_tests:conv_ops_3d_test \
+    -//tensorflow/python/kernel_tests:conv_ops_test \
     -//tensorflow/python/kernel_tests:dct_ops_test \
     -//tensorflow/python/kernel_tests:depthwise_conv_op_test \
     -//tensorflow/python/kernel_tests:fft_ops_test \
@@ -71,6 +71,7 @@ bazel test --test_sharding_strategy=disabled --config=rocm --test_tag_filters=-n
     -//tensorflow/python/kernel_tests:pooling_ops_3d_test \
     -//tensorflow/python/kernel_tests:pooling_ops_test \
     -//tensorflow/python/ops/parallel_for:control_flow_ops_test \
+    -//tensorflow/python/ops/parallel_for:gradients_test \
     -//tensorflow/python/profiler/internal:run_metadata_test \
     -//tensorflow/python/profiler:profile_context_test \
     -//tensorflow/python/profiler:profiler_test \
