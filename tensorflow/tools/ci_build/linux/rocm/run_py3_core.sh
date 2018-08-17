49e26d4d51b9982721d49ee88302b6dd612505c5
Wen-Heng (Jack) Chung <whchung@gmail.com>
2018-08-16 22:29:07 -0500

    Resolve merge conflicts for PR#126

--- tensorflow/tools/ci_build/linux/rocm/run_py3_core.sh
+++ tensorflow/tools/ci_build/linux/rocm/run_py3_core.sh
@@ -46,10 +46,7 @@ bazel test --test_sharding_strategy=disabled --config=rocm --test_tag_filters=-n
     -//tensorflow/python/estimator:linear_test \
     -//tensorflow/python/feature_column:feature_column_v2_test \
     -//tensorflow/python/keras:cudnn_recurrent_test \
-    -//tensorflow/python/keras:pooling_test \
-    -//tensorflow/python/keras:model_subclassing_test \
-    -//tensorflow/python/keras:normalization_test \
-    -//tensorflow/python/keras:training_gpu_test \
+    -//tensorflow/python/kernel_tests:batch_matmul_op_test \
     -//tensorflow/python/kernel_tests:conv1d_test \
     -//tensorflow/python/kernel_tests:conv2d_backprop_filter_grad_test \
     -//tensorflow/python/kernel_tests:conv_ops_3d_test \
@@ -57,6 +54,7 @@ bazel test --test_sharding_strategy=disabled --config=rocm --test_tag_filters=-n
     -//tensorflow/python/kernel_tests:dct_ops_test \
     -//tensorflow/python/kernel_tests:depthwise_conv_op_test \
     -//tensorflow/python/kernel_tests:fft_ops_test \
+    -//tensorflow/python/kernel_tests:matmul_op_test \
     -//tensorflow/python/kernel_tests:matrix_inverse_op_test \
     -//tensorflow/python/kernel_tests:matrix_triangular_solve_op_test \
     -//tensorflow/python/kernel_tests:pool_test \
