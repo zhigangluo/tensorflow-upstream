4d8a6b7b5c713b3acf5a1c841093a8dedb24b806
jcf94 <xff252595680@gmail.com>
2018-12-09 22:49:19 +0800

    Bug Fix for verbs

--- tensorflow/core/common_runtime/process_state.h
+++ tensorflow/core/common_runtime/process_state.h
@@ -75,6 +75,9 @@ class ProcessState : public ProcessStateInterface {
   // REQUIRES: must be called before GetCPUAllocator.
   void AddCPUFreeVisitor(SubAllocator::Visitor v);
 
+  const std::vector<SubAllocator::Visitor>& GetCPUAllocatorVisitor();
+  const std::vector<SubAllocator::Visitor>& GetCPUFreeVisitor();
+
   typedef std::unordered_map<const void*, MemDesc> MDMap;
 
  protected:
