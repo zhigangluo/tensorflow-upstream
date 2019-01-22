8ef46ea3113b14ad3a4c26a8bf0a93cb34a339b9
Wen-Heng (Jack) Chung <whchung@gmail.com>
2019-01-22 09:13:05 -0600

    Merge pull request #293 from jeffdaily/verbs-patch
    bring verbs up to date with latest upstream fixes

--- tensorflow/contrib/verbs/verbs_server_lib.cc
+++ tensorflow/contrib/verbs/verbs_server_lib.cc
@@ -82,6 +82,7 @@ std::once_flag reg_mem_visitors_call;
 
 Status VerbsServer::Init(ServiceInitFunction service_func,
                          RendezvousMgrCreationFunction rendezvous_mgr_func) {
+  std::call_once(reg_mem_visitors_call, []() { RdmaMgr::RegMemVisitors(); });
   Status s = GrpcServer::Init(service_func, rendezvous_mgr_func);
   {
     mutex_lock l(mu_);
@@ -93,9 +94,6 @@ Status VerbsServer::Init(ServiceInitFunction service_func,
     dynamic_cast<RdmaRendezvousMgr*>(worker_env()->rendezvous_mgr)
         ->SetRdmaMgr(rdma_mgr_);
   }
-  std::call_once(reg_mem_visitors_call, [this]() {
-    rdma_mgr_->RegMemVisitors();
-  });
   return s;
 }
 
